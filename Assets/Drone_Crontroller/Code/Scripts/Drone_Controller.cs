f4397bd191c46bbe80ee2e00ea12b54f33006608
Thomasdalsae <Thomasdalsae@gmail.com>
2023-10-02 02:02:25 +0200

    Drone done , still need to add torque and add moveing proppelers if wants to

--- Assets/Drone_Crontroller/Code/Scripts/Drone_Controller.cs
+++ Assets/Drone_Crontroller/Code/Scripts/Drone_Controller.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+
 namespace TdsWork
 {
     [RequireComponent(typeof(Drone_Inputs))]
@@ -15,9 +16,15 @@ namespace TdsWork
         [SerializeField]private float minMaxPitch = 30f;
         [SerializeField]private float minMaxRoll = 30f;
         [SerializeField]private float yawPower = 4f;
+        [SerializeField] private float lerpSpeed = 2f;
 
         private Drone_Inputs _input;
         private List<IEngine> _engines = new List<IEngine>();
+
+        private float _finalPitch;
+        private float _finalRoll;
+        private float _yaw;
+        private float _finalYaw;
         #endregion
 
         #region Main Methods
@@ -49,7 +56,17 @@ namespace TdsWork
 
         protected virtual void HandleControls()
         {
+            float pitch = _input.Cyclic.y * minMaxPitch;
+            float roll = -_input.Cyclic.x * minMaxRoll;
+            _yaw += _input.Pedals * yawPower;
+
+            _finalPitch = Mathf.Lerp(_finalPitch, pitch, Time.deltaTime * lerpSpeed);
+            _finalRoll = Mathf.Lerp(_finalRoll, roll, Time.deltaTime * lerpSpeed);
+            _finalYaw = Mathf.Lerp(_finalYaw, _yaw, Time.deltaTime * lerpSpeed);
             
+            Quaternion rot = Quaternion.Euler(_finalPitch,_finalYaw,_finalRoll);
+            //Add torque later
+            rb.MoveRotation(rot);
         }
         #endregion
     }
