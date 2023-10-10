d52c98df393f95dab145d14d574499d8fce0b3b3
Thomasdalsae <Thomasdalsae@gmail.com>
2023-10-10 22:16:00 +0200

    Almost done with adding checkpoint system base

--- Assets/TrackCheckpoints.cs
+++ Assets/TrackCheckpoints.cs
@@ -5,20 +5,51 @@ using UnityEngine;
 
 public class TrackCheckpoints : MonoBehaviour
 {
+    public event EventHandler OnPlayerCorrectCheckpoint;
+    public event EventHandler OnPlayerWrongCheckpoint;
+
+    [SerializeField] private List<Transform> DroneTransformList;
+    private List<CheckpointSingle> checkpointSingleList;
+    private List<int> nextCheckpointSingleIndexList;
     private void Awake()
     {
         Transform checkpointsTransform = transform.Find("Checkpoints");
 
+        checkpointSingleList = new List<CheckpointSingle>();
         foreach (Transform checkpointSingleTransform in checkpointsTransform)
         {
             CheckpointSingle checkpointSingle = checkpointSingleTransform.GetComponent<CheckpointSingle>();
             checkpointSingle.SetTrackCheckpoints(this);
+
+            checkpointSingleList.Add(checkpointSingle);
         }
-        
+
+        nextCheckpointSingleIndexList = new List<int>();
+
+        foreach (var carTransform  in DroneTransformList)
+        {
+            nextCheckpointSingleIndexList.Add(0);
+        }
+
     }
 
-    public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
+    public void DroneThroughCheckpoint(CheckpointSingle checkpointSingle, Transform droneTransform)
     {
-        Debug.Log(checkpointSingle.transform.name);
+        int nextCheckpointSingleIndex = nextCheckpointSingleIndexList[DroneTransformList.IndexOf(droneTransform)];
+        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex)
+        {
+            //Correct Checkpoint
+            Debug.Log("Correct");
+            
+            
+            nextCheckpointSingleIndexList[DroneTransformList.IndexOf(droneTransform)] = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
+            OnPlayerCorrectCheckpoint?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            //Wrong CheckPoint
+            Debug.Log("Wrong");
+            OnPlayerWrongCheckpoint?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
