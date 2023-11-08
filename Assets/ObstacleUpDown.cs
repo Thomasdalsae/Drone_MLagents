10ac7d8e062d74d3024bd73115cbcdfe1dd630b4
Thomasdalsae <Thomasdalsae@gmail.com>
2023-11-08 02:06:55 +0100

    Changed some of the parameters for reward

--- Assets/ObstacleUpDown.cs
+++ Assets/ObstacleUpDown.cs
@@ -9,37 +9,39 @@ public class NewBehaviourScript : MonoBehaviour
     #region Settings
 
     [SerializeField] private float Speed;
-    [SerializeField] private float Limit = 1;
+    [SerializeField] private float VerticalLimit = 1;
     [SerializeField] private Vector3 startLocation;
     #endregion
 
+    private bool movingUp = true;
 
-    
     private void Start()
     {
         startLocation = transform.localPosition;
-        transform.localPosition = transform.localPosition +  new Vector3(0, Random.Range(-1f, 1f),  Random.Range(-1f, 1f));
+        transform.localPosition = startLocation + new Vector3(0, Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         transform.localScale = new Vector3(Random.Range(10f, 15f), Random.Range(0.5f, 1f), Random.Range(0.3f, 1.2f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Limit += Time.deltaTime;
-        if (Limit >= 4f)
+        VerticalLimit += Time.deltaTime;
+
+        // Change direction when the VerticalLimit exceeds a threshold (e.g., 5 seconds)
+        if (VerticalLimit >= 5f)
         {
-            transform.localPosition += new Vector3(0,-Speed * Time.deltaTime, 0); 
+            movingUp = !movingUp;
+            VerticalLimit = 0;
+        }
+
+        // Move the object up or down based on the movingUp flag
+        if (movingUp)
+        {
+            transform.localPosition += new Vector3(0, Speed * Time.deltaTime, 0);
         }
         else
         {
-            
-            transform.localPosition += new Vector3(0,Speed * Time.deltaTime, 0); 
-        }
-
-        if (Limit >= 8)
-        {
-            
-        Limit = 0;
+            transform.localPosition += new Vector3(0, -Speed * Time.deltaTime, 0);
         }
     }
 }
