5d47980fa1cfe7fcce182b5aed607300c3079dba
Thomasdalsae <Thomasdalsae@gmail.com>
2023-11-13 21:16:58 +0100

    hmm

--- Assets/ObstacleUpDown.cs
+++ Assets/ObstacleUpDown.cs
@@ -11,6 +11,7 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private float Speed;
     [SerializeField] private float VerticalLimit = 1;
     [SerializeField] private Vector3 startLocation;
+    [SerializeField] private float rotationSpeed = 60f;
     #endregion
 
     private bool movingUp = true;
@@ -20,6 +21,8 @@ public class NewBehaviourScript : MonoBehaviour
         startLocation = transform.localPosition;
         transform.localPosition = startLocation + new Vector3(0, Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         transform.localScale = new Vector3(Random.Range(10f, 15f), Random.Range(0.5f, 1f), Random.Range(0.3f, 1.2f));
+        
+        rotationSpeed = (Random.Range(10f, 25f));
     }
 
     // Update is called once per frame
@@ -27,8 +30,9 @@ public class NewBehaviourScript : MonoBehaviour
     {
         VerticalLimit += Time.deltaTime;
 
+        transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
         // Change direction when the VerticalLimit exceeds a threshold (e.g., 5 seconds)
-        if (VerticalLimit >= 5f)
+        if (VerticalLimit >= 2.5f)
         {
             movingUp = !movingUp;
             VerticalLimit = 0;
